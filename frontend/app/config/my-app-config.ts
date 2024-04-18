4c76c97f5224f21f37401e209d1861c4128c5323
natalinorisafi <131485173+natris08@users.noreply.github.com>
2024-04-18 17:13:38 +0200

    Add files via upload

---
+++ frontend/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oad888b93R7SDekn5d7',
+        issuer: 'https://dev-48674114.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
